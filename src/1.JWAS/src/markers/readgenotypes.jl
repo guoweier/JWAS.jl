1fa3e02157c623974dca8ebb33488b024bcfaa63
guoweier <39327829+guoweier@users.noreply.github.com>
2018-06-12 17:28:25 -0700

    Update readgenotypes.jl

--- src/1.JWAS/src/markers/readgenotypes.jl
+++ src/1.JWAS/src/markers/readgenotypes.jl
@@ -1,19 +1,22 @@
-function readgenotypes(file::AbstractString;separator=' ',header=false,center=true)
+function readgenotypes(file::AbstractString;separator=',',header=false,center=true) 
+    #separator is ',' based on the default file format in JWAS_public_addgenotypes
     #println("The delimiters in file $file is ",separator,"  .")
 
     myfile = open(file)
     #get number of columns
-    row1   = split(readline(myfile),[separator,'\n'],keep=false)
+    row1   = split(readline(myfile),[separator,','],keep=false) #separator is ","
 
     if header==true
       markerID=row1[2:end]  #skip header
     else
-      markerID= ["NA"]
+      markerID= row1[:]
+      #assume header indicates the first column of row1
     end
+    
     #set types for each column and get number of markers
     ncol= length(row1)
     etv = Array{DataType}(ncol)
-    fill!(etv,Float64)
+    fill!(etv,Float64) 
     etv[1]=String
     close(myfile)
 
@@ -21,8 +24,8 @@ function readgenotypes(file::AbstractString;separator=' ',header=false,center=tr
     df = CSV.read(file, types=etv, delim = separator, header=header)
 
 
-    obsID=convert(Array,df[:,1])
-    genotypes = map(Float64,convert(Array,df[:,2:end]))
+    obsID=convert(Array,df[2:end,1])
+    genotypes = map(Float64,convert(Array,df[2:end,2:end])) 
     nObs,nMarkers = size(genotypes)
 
     if center==true
@@ -38,9 +41,9 @@ end
 
 #M is of type: Array{Float64,2} or DataFrames
 function readgenotypes(M::DataFrames.DataFrame;header=false,separator=' ',center=true)#improve later
-    genotypes  = map(Float64,convert(Array,M))
-    obsID     = ["NA"]
-    markerID  = ["NA"]
+    genotypes  = map(Float64,convert(Array,M[:,2:end]))
+    markerID = ["NA"] #cannot find a method for extracting columns names in dataframes
+    obsID = convert(Array,M[:,1]) 
     nObs,nMarkers = size(genotypes)
 
     if center==true
